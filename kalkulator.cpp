55e07343869f516f9b7ddfc0eee688f08d98a971
BartekHP <bartoszmielewczyk@outlook.com>
2022-10-13 11:15:14 +0200

    PoP

---
+++ kalkulator.cpp
@@ -0,0 +1,54 @@
+#include "iostream"
+#include "math.h"
+
+using namespace std;
+
+int main(){
+    int a,b;
+    double wynik;
+    char znak;
+    cout << "podaj liczby" << endl;
+    cin >> a >> b;
+    if (a<1000 && a>-1000){
+        if (b<1000 && b>-1000){
+            cout << "podaj znak dzialania" << endl;
+            cin >> znak;
+            switch (znak) {
+                case '+':
+                    wynik = a+b;
+                    cout << "Wynik dzialania to:" << wynik;
+                    break;
+                case '-':
+                    wynik = a - b;
+                    cout << "Wynik dzialania to: " << wynik;
+                    break;
+                case '*':
+                    wynik = a*b;
+                    cout << "Wynik dzialania to: " << wynik;
+                    break;
+                case '/':
+                    if (b!=0) {
+                        wynik = a / b;
+                        cout << "Wynik dzialania to: " << wynik;
+                        break;
+                    } else {
+                        cout << "Nie mozna dzielic przez zero";
+                        break;
+                    }
+                case '%':
+                    if (b!=0){
+                        cout << "Wynik dzialania to: " << a%b;
+                        break;
+                    } else {
+                        cout << "Nie mozna dzielic przez zero";
+                        break;
+                    }
+
+            }
+        } else {
+            cout << "Niewlasciwe liczby";
+        }
+    } else {
+        cout << "Niewlasciwe liczby";
+    }
+}
